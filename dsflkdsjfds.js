42094413af2fa41724662fc7f60f6bf113ee87e2
fredqueiroz <freedqueirooz@gmail.com>
2019-03-18 16:06:34 +0800

    Create dsflkdsjfds.js

---
+++ dsflkdsjfds.js
@@ -0,0 +1,29 @@
+
+var i = 1;
+var j = 0;
+var text = document.querySelector('#buttons').innerHTML;
+var buttons = text.split('<');
+var id = '';
+var value = '';
+var value2 = document.querySelector('#myaccordion').innerHTML;
+for (i = 1; i <= buttons.length - 1; i++)
+{ value = value + '<div id=\"div' + i + '\"><' + buttons[i] + '</div>';
+value2 = value2.replace('<li>', '<li class=\"spotli\"><a class=\"spota\" data-id=\"div' + i + '\" href=\"#\"><span class=\"filter-names\">');
+ }
+
+ $('#buttons').html(value);
+ value2 = value2.replace(/<\/li>/g, '</span></a></li>');
+ $('#myaccordion').html(value2);
+
+function triggerClick(id) {
+	var children = $("#" + id).children();
+	children.click();
+}
+$(".spota").click(function() {
+             var navMenu = $(this).attr('data-id');
+                triggerClick(navMenu);   
+});
+$(".icons").click(function() {
+                var navMenu = $(this).attr('data-id');
+                triggerClick(navMenu);
+});
