2fb71cb24ec5bd8db688f480ee9ee2010ca3dbcc
fredqueiroz <freedqueirooz@gmail.com>
2019-03-18 17:12:16 +0800

    Update dsflkdsjfds.js

--- dsflkdsjfds.js
+++ dsflkdsjfds.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function() {
 var i = 1;
 var j = 0;
 var text = document.querySelector('#buttons').innerHTML;
@@ -164,3 +164,4 @@ $(".ui-accordion .ui-accordion-header").css({
 
 
 
+});
